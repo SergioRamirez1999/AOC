4236ce51c3ecceec95744190990f3ce10826c84f
Sergio Ramirez <ramirezsergiod1999@outlook.com>
2023-12-02 00:11:14 -0300

    Dia 01 completo

---
+++ src/main/java/me/sergioramirez/days/day01/Puzzle.java
@@ -0,0 +1,84 @@
+package me.sergioramirez.days.day01;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Puzzle
+{
+
+    public static List<String> read_all_lines(String filepath) {
+        List<String> all_lines = new ArrayList<>();
+        try {
+            all_lines = Files.readAllLines(Paths.get(filepath), StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return all_lines;
+    }
+
+    public static int count_calibration_A(List<String> input) {
+
+        return input.stream().map(l -> l.replaceAll("[\\D]", ""))
+                .map(
+                    l -> {
+                        if(!l.isEmpty())
+                            return ""+l.charAt(0
+                            )+l.charAt(l.length()-1);
+                        else
+                            return "0";
+                    }
+                ).mapToInt(Integer::parseInt).sum();
+    }
+
+    public static int count_calibration_B(List<String> input) {
+        List<String> posibilities = List.of("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "1", "2", "3", "4", "5", "6", "7", "8", "9");
+        List<List<String>> calibration_values = new ArrayList<>();
+        for(String line: input) {
+            List<String> matches = new ArrayList<>();
+            String auxLine = line;
+            while(!auxLine.isEmpty()) {
+                StringBuffer subString = new StringBuffer();
+                for(int i = 0; i < auxLine.length(); i++) {
+                    subString.append(auxLine.charAt(i));
+                    Optional<String> ocurrence = posibilities.stream().filter(p -> subString.toString().contains(p)).findFirst();
+                    if(ocurrence.isPresent()) {
+                        matches.add(ocurrence.get());
+                        if(subString.toString().matches(".*[0-9].*"))
+                            i++;
+                        auxLine = auxLine.substring(i);
+                        break;
+                    } else if(i == auxLine.length()-1)
+                        auxLine = "";
+                }
+            }
+            calibration_values.add(matches);
+        }
+
+
+        return calibration_values.stream().map(
+                    e -> Stream.of(e.get(0), e.get(e.size()-1)).map(x -> {
+                        if(x.matches(".*[0-9].*"))
+                            return x;
+                        else
+                            return posibilities.get(posibilities.indexOf(x)+9);
+                    }
+                ).collect(Collectors.joining())).mapToInt(Integer::valueOf).sum();
+    }
+
+    public static void main( String[] args ) {
+        String filepath = Paths.get("").toAbsolutePath()+"\\src\\main\\java\\me\\sergioramirez\\days\\day01\\input.txt";
+        List<String> input_lines = read_all_lines(filepath);
+        int result_A = count_calibration_A(input_lines);
+        System.out.println(result_A);
+
+        int result_B = count_calibration_B(input_lines);
+        System.out.println(result_B);
+
+    }
+}
---
+++ src/test/java/me/sergioramirez/PuzzleTest.java
@@ -0,0 +1,38 @@
+package me.sergioramirez;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class PuzzleTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public PuzzleTest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( PuzzleTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
