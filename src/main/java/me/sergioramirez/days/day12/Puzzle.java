f2156bf1cc7475a620b9a39ff6513fca142d60a6
Sergio Ramirez <ramirezsergiod1999@outlook.com>
2023-12-13 19:56:10 -0300

    Dia 12 incompleto

---
+++ src/main/java/me/sergioramirez/days/day12/Puzzle.java
@@ -0,0 +1,19 @@
+package me.sergioramirez.days.day12;
+
+import me.sergioramirez.util.InputTextUtil;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Puzzle {
+
+    private static final String filepath = Paths.get("").toAbsolutePath()+"\\src\\main\\java\\me\\sergioramirez\\days\\day12\\input.txt";
+    private static final List<String> input = InputTextUtil.read_all_lines(filepath);
+
+
+    public static void main(String[] args) {
+
+    }
+}
