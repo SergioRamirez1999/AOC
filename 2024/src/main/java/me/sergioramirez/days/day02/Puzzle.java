288828b9952f7355139c68ce86760859c817bd12
Sergio Ramirez <ramirezsergiod1999@outlook.com>
2024-12-03 00:53:31 -0300

    Se termina day 02

--- 2024/src/main/java/me/sergioramirez/days/day02/Puzzle.java
+++ 2024/src/main/java/me/sergioramirez/days/day02/Puzzle.java
@@ -31,46 +31,76 @@ public class Puzzle
         int maxDifference = 3;
 
         for(List<Integer> report: reports) {
-            Integer nLeft = null;
-            boolean isSafeReport = true;
-            boolean isIncremental = true;
-            int index = 0;
-            for(Integer nActual: report) {
-                if(nLeft == null) {
-                    nLeft = nActual;
-                    index++;
-                    continue;
-                }
-                if(nActual > nLeft) { //Incremental
-                    if(index > 1 && !isIncremental) {
-                        isSafeReport = false;
-                        break;
-                    }
-                    isIncremental = true;
-                }
-
-                if(nActual < nLeft) { //Decremental
-                    if(index > 1 && isIncremental) {
-                        isSafeReport = false;
-                        break;
-                    }
-                    isIncremental = false;
-                }
-
-                if(!isBetween(Math.abs(nLeft - nActual), minDifference, maxDifference)) {
-                    isSafeReport = false;
-                    break;
-                }
-
-                nLeft = nActual;
-                index++;
-            }
-            if(isSafeReport) {
+            if(isSafeReport(report, minDifference, maxDifference))
                 safeReports++;
+        }
+
+        return safeReports;
+    }
+
+
+    public static int count_safe_tolerance_reports() {
+        int safeReports = 0;
+
+        int minDifference = 1;
+        int maxDifference = 3;
+
+        for(List<Integer> report: reports) {
+            if(isSafeReport(report, minDifference, maxDifference))
+                safeReports++;
+            else {
+                int size = report.size();
+                boolean isSafe = false;
+                for(int i=0; i<size && !isSafe; i++) {
+                    List<Integer> reportAux = new ArrayList<>(report);
+                    reportAux.remove(i);
+                    if(isSafeReport(reportAux, minDifference, maxDifference)) {
+                        safeReports++;
+                        isSafe = true;
+                    }
+                }
             }
         }
 
         return safeReports;
+    }
+
+    private static boolean isSafeReport(List<Integer> report, int minDifference, int maxDifference) {
+        Integer nLeft = null;
+        boolean isSafeReport = true;
+        boolean isIncremental = true;
+        int index = 0;
+        for(Integer nActual: report) {
+            if(nLeft == null) {
+                nLeft = nActual;
+                index++;
+                continue;
+            }
+            if(nActual > nLeft) { //Incremental
+                if(index > 1 && !isIncremental) {
+                    isSafeReport = false;
+                    break;
+                }
+                isIncremental = true;
+            }
+
+            if(nActual < nLeft) { //Decremental
+                if(index > 1 && isIncremental) {
+                    isSafeReport = false;
+                    break;
+                }
+                isIncremental = false;
+            }
+
+            if(!isBetween(Math.abs(nLeft - nActual), minDifference, maxDifference)) {
+                isSafeReport = false;
+                break;
+            }
+
+            nLeft = nActual;
+            index++;
+        }
+        return isSafeReport;
     }
 
     public static boolean isBetween(int number, int min, int max) {
@@ -82,5 +112,7 @@ public class Puzzle
     public static void main( String[] args ) {
         int result_A = count_safe_reports();
         System.out.println(result_A);
+        int result_B = count_safe_tolerance_reports();
+        System.out.println(result_B);
     }
 }
